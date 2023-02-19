387ee915c3c4ee9538f54db66fdddc805059b735
Rezgui Aziz <rezgui01aziz@gmail.com>
2023-02-19 13:16:48 +0100

    add picture to user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -57,6 +57,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\OneToMany(mappedBy: 'user', targetEntity: Rating::class)]
     private Collection $id_rating;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $picture = null;
+
     public function __construct()
     {
         $this->id_abonnement = new ArrayCollection();
@@ -298,6 +301,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
                 $idRating->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
 
         return $this;
     }
