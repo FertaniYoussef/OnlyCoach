d5d3b6aaea9caad937a0781a49b69f6ddc6d60df
FertaniYoussef <fertaniyoussef23@gmail.com>
2023-02-22 16:40:38 +0100

    Added checkout page

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -25,7 +25,7 @@ class AbonnementController extends AbstractController
         ]);
     }
 
-    #[Route('/abonnement/{coachId}', name:'subscribe_to_coach')]
+/*     #[Route('/abonnement/{coachId}', name:'subscribe_to_coach')]
     public function subscribeToCoach(Request $request, $coachId,UserRepository $userrepo,CoachRepository $coachrepo,CategorieRepository $catrepo,ManagerRegistry $doctrine)
     {
         $user = $this->getUser();
@@ -36,7 +36,7 @@ class AbonnementController extends AbstractController
         // Check if the user is already subscribed to the coach
        /*  if ($user->isSubscribedTo($coach)) {
             throw $this->createNotFoundException('The coach exist');
-        } */
+        } 
         $subscription = new Abonnement();
         $subscription->setDateDeb(new \DateTime());
         $endDate = (new \DateTime())->modify('+30 days');
@@ -46,14 +46,14 @@ class AbonnementController extends AbstractController
         $subscription->setCoach($coach);
         $user-> addIdAbonnement($subscription);
       
-          // Save the subscription to the database
+         
           $entityManager = $doctrine->getManager();
           $entityManager->persist($subscription);
           $entityManager->flush();
           
-            // Redirect the user to a page confirming the subscription
-            return $this->render('abonnement/index.html.twig');
-    }
+          
+            return $this->render('abonnement/index.html.twig'); 
+    }*/
     #[Route('/cancel/{subscriptionid}',name:'unsubscribe_from_coach')]
         public function unsubscribeToCoach(Request $request, $subscriptionid,AbonnementRepository $aborepo,ManagerRegistry $doctrine)
         {
@@ -70,10 +70,15 @@ class AbonnementController extends AbstractController
             die();
         }
 
-     #[Route('/abonnement',name:'unsubscribe_from_coach')]
-    public function subscriptionConfirmation(): Response
+     #[Route('/abonnement/{coachId}',name:'subscribe_checkout')]
+    public function subscriptionConfirmation(Request $request, $coachId,UserRepository $userrepo,CoachRepository $coachrepo,CategorieRepository $catrepo,ManagerRegistry $doctrine): Response
     {
+        $user = $this->getUser();
+        $coach =$coachrepo->find($coachId);
+          if (!$coach) {
+              throw $this->createNotFoundException('The coach does not exist');
+          }
         // Render a template confirming the subscription
-        return $this->render('abonnement/index.html.twig');
+        return $this->render('abonnement/index.html.twig',array('coach'=>$coach));
     }
 }
