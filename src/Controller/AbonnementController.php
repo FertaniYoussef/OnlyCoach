5c9ec76e5e7b2cf6004d81adcbb811731e10d09a
FertaniYoussef <fertaniyoussef23@gmail.com>
2023-02-23 18:22:02 +0100

    CHECKOUT COMPLETED

--- src/Controller/AbonnementController.php
+++ src/Controller/AbonnementController.php
@@ -14,9 +14,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-use Stripe\Stripe;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Stripe\Checkout\Session;
+use Stripe;
 
 class AbonnementController extends AbstractController
 {
@@ -24,11 +23,11 @@ class AbonnementController extends AbstractController
     public function index(): Response
     {
         return $this->render('abonnement/index.html.twig', [
-            'controller_name' => 'AbonnementController',
+            'stripe_key' => $_ENV["STRIPE_KEY"],
         ]);
     }
 
-/*     #[Route('/abonnement/{coachId}', name:'subscribe_to_coach')]
+  #[Route('/abonnement/{coachId}/checkout/success', name:'success')]
     public function subscribeToCoach(Request $request, $coachId,UserRepository $userrepo,CoachRepository $coachrepo,CategorieRepository $catrepo,ManagerRegistry $doctrine)
     {
         $user = $this->getUser();
@@ -39,7 +38,7 @@ class AbonnementController extends AbstractController
         // Check if the user is already subscribed to the coach
        /*  if ($user->isSubscribedTo($coach)) {
             throw $this->createNotFoundException('The coach exist');
-        } 
+        }  */
         $subscription = new Abonnement();
         $subscription->setDateDeb(new \DateTime());
         $endDate = (new \DateTime())->modify('+30 days');
@@ -54,9 +53,15 @@ class AbonnementController extends AbstractController
           $entityManager->persist($subscription);
           $entityManager->flush();
           
-          
-            return $this->render('abonnement/index.html.twig'); 
-    }*/
+          $this->addFlash(
+            'success',
+            'Payment Succesful'
+        );
+            return $this->render('abonnement/success.html.twig', [
+                'stripe_key' => $_ENV["STRIPE_KEY"],
+                'coach'=> $coach
+            ]); 
+    }
     #[Route('/cancel/{subscriptionid}',name:'unsubscribe_from_coach')]
         public function unsubscribeToCoach(Request $request, $subscriptionid,AbonnementRepository $aborepo,ManagerRegistry $doctrine)
         {
@@ -66,13 +71,19 @@ class AbonnementController extends AbstractController
             if (!$abonnement) {
                 throw $this->createNotFoundException('The subscription does not exist');
             }        
-            
+            $user->removeIdAbonnement($abonnement);
             $em = $doctrine->getManager();
             $em->remove($abonnement);
             $em->flush();
-            die();
+          
         }
-
+        #[Route('/abonnement/{coachId}/checkout/failed', name: 'failure')]
+        public function failedPayment(): Response
+        {
+            return $this->render('main/index.html.twig', [
+                'stripe_key' => $_ENV["STRIPE_KEY"],
+            ]);
+        }
      #[Route('/abonnement/{coachId}',name:'subscribe_coach')]
     public function subscriptionConfirmation(Request $request, $coachId,UserRepository $userrepo,CoachRepository $coachrepo,CategorieRepository $catrepo,ManagerRegistry $doctrine): Response
     {
@@ -82,36 +93,43 @@ class AbonnementController extends AbstractController
               throw $this->createNotFoundException('The coach does not exist');
           }
         // Render a template confirming the subscription
-        return $this->render('abonnement/index.html.twig',array('coach'=>$coach));
+        return $this->render('abonnement/index.html.twig', [
+            'stripe_key' => $_ENV["STRIPE_KEY"],
+            'CLIENT_SECRET'=>$_ENV['STRIPE_SECRET'],
+            'coach'=> $coach
+        ]); 
     }
-    #[Route('/abonnement/{coachId}/checkout',name:'subscribe_checkout')]
+  
+    #[Route('/abonnement/{coachId}/checkout',name:'subscribe_checkout',methods:['POST'])]
     public function checkout(Request $request, $coachId,CoachRepository $coachrepo) : Response
     {
         $coach =$coachrepo->find($coachId);
         if (!$coach) {
             throw $this->createNotFoundException('The coach does not exist');
         }
+$total= $coach->getPrix() * 1.1 ;
+$formattedPrice = number_format($total, 2, '.', '');
+        Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+        try {
+            Stripe\Charge::create ([
+                "amount" => $formattedPrice * 100,
+                "currency" => "usd",
+                "source" => $request->request->get('stripeToken'),
+                "description" => " Payment Test"
+            ]);
 
-        Stripe::setApiKey('sk_test_51MeLAvEYkEbKW1v6I2M4WnJtf1Ts5kfNlJr3q2XnaUxJ2MwGbYhjq6tOxutQvAyBXqUYXmjm6DaHAPDw1c1JuyS300btIzyOVi');
-         $session = Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [
-                [
-                    'price_data' => [
-                        'currency' => 'usd',
-                        'unit_amount' => $coach->getPrix() * 100, // Stripe uses cents as the currency unit
-                        'product_data' => [
-                            'name' => $coach->getNom(),
-                            'description' => $coach->getDescription(),
-                        ],
-                    ],
-                    'quantity' => 1,
-                ],
-            ],
-            'mode' => 'payment',
-            'success_url' => $this->generateUrl('subscription_checkout_success', [], true),
-            'cancel_url' => $this->generateUrl('subscription_checkout_cancel', [], true),
-        ]);
+            // Paiement réussi, on redirige l'utilisateur vers la page de succès
+        
+         
+            return $this->redirectToRoute('success', ["coachId"=>$coachId], Response::HTTP_SEE_OTHER);
+        } catch (Stripe\Exception\CardException $e) {
+            // Paiement échoué, on redirige l'utilisateur vers la page d'échec
+            $this->addFlash(
+                'failure',
+                'Payment Succesful'
+            );
+            return $this->redirectToRoute('faillure', ["coachId"=>$coachId], Response::HTTP_SEE_OTHER);
+        }
           
 
     }
