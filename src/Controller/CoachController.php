a5f2352887e2899ea09e2f70503c0f5b6e17454f
fatmaesprit2023 <125260896+fatmaesprit2023@users.noreply.github.com>
2023-03-08 15:58:19 +0100

    favoris

--- src/Controller/CoachController.php
+++ src/Controller/CoachController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Coach;
+use App\Entity\Abonnement;
 use App\Repository\CoachRepository;
 use App\Repository\CategorieRepository;
+use App\Repository\AbonnementRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Doctrine\Persistence\ManagerRegistry;
 
 
 
@@ -18,12 +22,63 @@ class CoachController extends AbstractController
 {
     #[Route('/coachs', name: 'app_coach')]
     
-    public function index(CoachRepository $coachRepository): Response
+    public function index(CoachRepository $coachRepository,AbonnementRepository $abonnementRepository): Response
     {
+        $abon=$abonnementRepository->findAll();
+
         return $this->render('coach/index.html.twig', [
-            'coaches' => $coachRepository->findAll(),'userinfo'=>$this->getUser()
+            'coaches' => $coachRepository->findAll(),'userinfo'=>$this->getUser(),'abon'=>$abon
         ]);
     }
+
+    #[Route('/coachs/fav/{id}', name: 'app_coach_fav')]
+    
+    public function inverFav(CoachRepository $coachRepository,AbonnementRepository $abonnementRepository,int $id,ManagerRegistry $doctrine): Response
+    {
+        $user = $this->getUser();
+        $abonnement=$abonnementRepository->findAbonnementByAdherentAndCoach($user->getId(),$id);
+        $abonnement->setisFav(!$abonnement->isisFav());
+        $entityManager = $doctrine->getManager();
+        $entityManager=$doctrine->getManager();
+        $entityManager->persist($abonnement);
+        $entityManager->flush();
+        $abon=$abonnementRepository->findAll();
+        $coachRepository = $entityManager->getRepository(Coach::class);
+
+        $query = $coachRepository->createQueryBuilder('c')
+            ->join('c.id_abonnement', 'a')
+            ->where('a.user = :userId')
+            ->setParameter('userId',$user->getId() )
+            ->getQuery();
+
+        $coaches = $query->getResult();
+        return $this->render('coach/fav.html.twig', [
+            'coaches' => $coaches,'userinfo'=>$this->getUser(),'abon'=>$abon
+        ]);
+    }
+
+
+    #[Route('/coachs/fave/home', name: 'app_coach_fav_home')]
+    
+    public function HomeFav(CoachRepository $coachRepository,AbonnementRepository $abonnementRepository,ManagerRegistry $doctrine): Response
+    {
+        $user = $this->getUser();
+        $abon=$abonnementRepository->findAll();
+        $entityManager = $doctrine->getManager();
+        $coachRepository = $entityManager->getRepository(Coach::class);
+
+        $query = $coachRepository->createQueryBuilder('c')
+            ->join('c.id_abonnement', 'a')
+            ->where('a.user = :userId')
+            ->setParameter('userId',$user->getId() )
+            ->getQuery();
+
+        $coaches = $query->getResult();
+        return $this->render('coach/fav.html.twig', [
+            'coaches' => $coaches,'userinfo'=>$this->getUser(),'abon'=>$abon
+        ]);
+    }
+
     #[Route('/coachs/{id}', name: 'app_coach_tri')]
         public function indexcategorie($id,CoachRepository $coachRepository, CategorieRepository $categorieRepository): Response
         {
