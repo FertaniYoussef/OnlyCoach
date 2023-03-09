b58c1e252505d50dee398e83e659d57d69f1f7d0
FertaniYoussef <fertaniyoussef23@gmail.com>
2023-03-09 21:37:31 +0100

    Deleted pdfCoachController

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -64,7 +64,45 @@ class PdfController extends AbstractController
         ]);
         return $response;
     }
-    #[Route('/coach/dashboard/generateinvoice/?', name: 'app_coach_pdf')]
+    #[Route('/admin/dashboard/pdf', name: 'app_coach_pdf')]
+    public function index()
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('default/liste.html.twig', [
+            'title' => "Welcome to our PDF Test"
+        ]);
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Store PDF Binary Data
+        $output = $dompdf->output();
+        
+        // In this case, we want to write the file in the public directory
+        $publicDirectory = $this->get('kernel')->getProjectDir() . '/public';
+        // e.g /var/www/project/public/mypdf.pdf
+        $pdfFilepath =  $publicDirectory . '/mypdf.pdf';
+        
+        // Write file to the desired path
+        file_put_contents($pdfFilepath, $output);
+        
+        // Send some text response
+        return new Response("The PDF file has been succesfully generated !");
+    }
+    #[Route('/coach/dashboard/generateinvoice/?', name: 'app_coach_invoice_pdf')]
     public function coach_pdf(Request $request,ManagerRegistry $doctrine,CoachRepository $coachrepo,UserRepository $userrepo): Response
     {   
 
